7415fc60d4d154d6be8fc592e1c436d3c5c752d3
FatBabyYoda <111348190+FatBabyYoda@users.noreply.github.com>
2024-04-16 19:09:53 +0200

    Update OrdrarHattar.java

--- src/Hattmakarsystem/OrdrarHattar.java
+++ src/Hattmakarsystem/OrdrarHattar.java
@@ -18,29 +18,57 @@ import oru.inf.InfException;
  * @author Lucas
  */
 public class OrdrarHattar {
-    
-    
+    static ArrayList<HashMap<String,String>> hattarspec;
+    static ArrayList<HashMap<String,String>> hattarlagger;
+    static HashMap<String,String> order;
     public static void fyllOrderLista(JList hattar,JLabel ordernummer,JLabel pris,JLabel datum)
     {
         DefaultListModel<String> listModel = new DefaultListModel<>();
         Databaskoppling.koppling();
         try {
-            HashMap<String,String> order = Databaskoppling.idb.fetchRow("select * from ordrar where orderID = "+ordernummer.getText());
-            
-            ArrayList<HashMap<String,String>> hattarspec = Databaskoppling.idb.fetchRows("select * from specialorderkoppling join special on specialorderkoppling.specialID = special.specialID where orderID = "+ordernummer.getText());
-            ArrayList<HashMap<String,String>> hattarlagger = Databaskoppling.idb.fetchRows("select * from lagerorderkoppling join lagerforda on lagerorderkoppling.lagfordID = lagerforda.lagfordID where orderID = "+ordernummer.getText());
-           
+            order = Databaskoppling.idb.fetchRow("select * from ordrar where orderID = "+ordernummer.getText());           
+            hattarspec = Databaskoppling.idb.fetchRows("select * from specialorderkoppling join special on specialorderkoppling.specialID = special.specialID where orderID = "+ordernummer.getText());
+            hattarlagger = Databaskoppling.idb.fetchRows("select * from lagerorderkoppling join lagerforda on lagerorderkoppling.lagfordID = lagerforda.lagfordID where orderID = "+ordernummer.getText());
+           /*
             listModel.addElement("Special Orderar:");
             for (HashMap<String, String> specialOrder : hattarspec) {
                 
                     listModel.addElement("antal" + ": " + specialOrder.get("antal")+" storlek" + ": " + specialOrder.get("storlek")+" pris" + ": " + specialOrder.get("pris")+" dekoration" + ": " + specialOrder.get("dekoration")+" namn" + ": " + specialOrder.get("namn")+" beskrivning" + ": " + specialOrder.get("beskrivning")+" ID" + ": " + specialOrder.get("specialID"));
                 
+            }*/
+            //förbättrings kod som jag inte implementeraat än tar bort null värden och mer lätt läslig
+            String value = "";
+            listModel.addElement("Special Orderar:");
+            for (HashMap<String, String> stockedOrder : hattarlagger) {
+                for (String key : stockedOrder.keySet()) {
+                    if (stockedOrder.get(key) != null && !key.equals("lagfordID")) {
+                       value += key + ": " + stockedOrder.get(key) + " | ";
+                       
+                    }
+                           
+                }
+                listModel.addElement(value);
             }
+            value = "";
+            listModel.addElement("lager Orderar:");
+            for (HashMap<String, String> stockedOrder : hattarspec) {
+                for (String key : stockedOrder.keySet()) {
+                    if (stockedOrder.get(key) != null && !key.equals("lagfordID")) {
+                       value += key + ": " + stockedOrder.get(key)+ " | ";
+                       
+                    }
+                           
+                }
+                listModel.addElement(value);
+            }
+
+            /*
              listModel.addElement("lager Orderar:");
             for (HashMap<String, String> lagerOrder : hattarlagger) {
             listModel.addElement("antal" + ": " + lagerOrder.get("antal")+" storlek" + ": " + lagerOrder.get("storlek")+" pris" + ": " + lagerOrder.get("pris")+" dekoration" + ": " + lagerOrder.get("dekoration")+" namn" + ": " + lagerOrder.get("namn")+" beskrivning" + ": " + lagerOrder.get("beskrivning")+" ID" + ": " + lagerOrder.get("lagfordID"));
 
             }
+            */
             hattar.setModel(listModel);
             pris.setText(order.get("totalpris")+" kr");
             datum.setText(order.get("datum"));
