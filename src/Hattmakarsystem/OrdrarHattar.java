b30116d8d392469a7b745a7b015757254738b59a
FatBabyYoda <111348190+FatBabyYoda@users.noreply.github.com>
2024-04-18 10:47:47 +0200

    Update OrdrarHattar.java

--- src/Hattmakarsystem/OrdrarHattar.java
+++ src/Hattmakarsystem/OrdrarHattar.java
@@ -48,6 +48,7 @@ public class OrdrarHattar {
                            
                 }
                 listModel.addElement(value);
+                value = "";
             }
             value = "";
             
@@ -61,6 +62,7 @@ public class OrdrarHattar {
                            
                 }
                 listModel.addElement(value);
+                value = "";
             }
 
             /*
