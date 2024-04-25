01b4749733107e34c20c72c54395a2606fe9dd27
FilipGouriye <154084439+FilipGouriye@users.noreply.github.com>
2024-04-25 12:39:00 +0200

    kjn

--- src/Hattmakarsystem/LaggTillAnpassade.java
+++ src/Hattmakarsystem/LaggTillAnpassade.java
@@ -13,26 +13,44 @@ import oru.inf.InfException;
  * @author filip
  */
 public class LaggTillAnpassade {
-    
-    public void laggTillAnpassade(String namn, int pris, String dekoration, String beskrivning, int storlek, JCheckBox jCheckBox){
-        
-        try{
-         Databaskoppling.koppling();
-         
-         if(jCheckBox.isSelected()){
-         String fraga = "SELECT MAX(anpassadID) AS senaste_anpassad_id FROM anpassade";
-         String senasteIdStr = Databaskoppling.idb.fetchSingle(fraga);
-         int senasteId = senasteIdStr != null ? Integer.parseInt(senasteIdStr) + 1 : 1;   
-            
-         String laggTill = "INSERT INTO anpassade (anpassadId, namn, pris, storlek, dekoration, beskrivning) VALUES ('" + senasteId + "', '" + namn + "', '" + pris + "', '" + storlek + "', '" + dekoration + "', '" + beskrivning + "')";
-         Databaskoppling.idb.insert(laggTill);
-            
-         JOptionPane.showMessageDialog(null, "Ny anpassad hatt tillagd!");
-         }
-         
-        }catch(InfException e){
+
+    public void laggTillAnpassade(String namn, int pris, String dekoration, String beskrivning, int storlek, JCheckBox jCheckBox) {
+
+        try {
+            Databaskoppling.koppling();
+
+            if (jCheckBox.isSelected()) {
+                String fraga = "SELECT MAX(anpassadID) AS senaste_anpassad_id FROM anpassade";
+                String senasteIdStr = Databaskoppling.idb.fetchSingle(fraga);
+                int senasteId = senasteIdStr != null ? Integer.parseInt(senasteIdStr) + 1 : 1;
+                String laggTill = "INSERT INTO anpassade (anpassadId, namn, pris, storlek, dekoration, beskrivning) VALUES ('" + senasteId + "', '" + namn + "', '" + pris + "', '" + storlek + "', '" + dekoration + "', '" + beskrivning + "')";
+                Databaskoppling.idb.insert(laggTill);
+
+                JOptionPane.showMessageDialog(null, "Ny anpassad hatt tillagd!");
+            }
+
+        } catch (InfException e) {
             JOptionPane.showMessageDialog(null, "fel i databasen");
             System.out.println("Fel i databasen" + e.getMessage());
-        }    
-    }  
+        }
+    }
+
+    public void laggTillAnpassadOrder(int totalpris, String epost, String datum) {
+
+        try {
+            String fraga2 = "SELECT MAX(orderID) AS senaste_order_id FROM order";
+            String senasteIdStr2 = Databaskoppling.idb.fetchSingle(fraga2);
+            int senasteId2 = senasteIdStr2 != null ? Integer.parseInt(senasteIdStr2) + 1 : 1;
+            String kund = "SELECT kundID from kund where email = '" + epost + "'";
+            Databaskoppling.idb.fetchColumn(kund);
+            String laggTill2 = "INSERT INTO order (orderID, status, datum, totalpris, anvandare, kund) VALUES ('" + senasteId2 + "', '" + '0' + "', '" + datum + "', NULL, '" + kund + "')";
+            Databaskoppling.idb.insert(laggTill2);
+            
+            JOptionPane.showMessageDialog(null, "Order tillagd!");
+            
+        } catch (InfException e) {
+          JOptionPane.showMessageDialog(null, "fel i databasen");
+          System.out.println("Fel i databasen" + e.getMessage());
+        }
+    }
 }
